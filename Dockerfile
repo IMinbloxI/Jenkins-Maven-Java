3a61eafe8d0612aceaa7ffb6ccfa29c2b4f99b71
Your Name <you@example.com>
2026-09-24 23:32:40 +0700

    Fix duplicate agent declaration

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM eclipse-temurin:17-jre
+FROM eclipse-temurin:21-jre
 COPY target/my-app-1.0-SNAPSHOT.jar app.jar
 CMD ["java", "-jar", "app.jar"]
--- jenkins/start.sh
+++ jenkins/start.sh
@@ -1,6 +1,8 @@
-cat > ~/jenkins/start.sh << 'EOF'
+cat -n ./jenkins/start.sh
+
 #!/bin/bash
-cd ~/jenkins
-java -Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true -jar jenkins.war --httpPort=9090
-EOF
-chmod +x ~/jenkins/start.sh
+cd ./jenkins
+java -Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true \
+     -Djenkins.install.runSetupWizard=false \
+     -jar jenkins.war --httpPort=9090
+
